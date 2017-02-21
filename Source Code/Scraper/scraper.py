982e37ed81e600061407142567d5c4e489ed1aa0
Alec Schule <aschule@usc.edu>
2017-02-20 20:20:02 -0800

    initial scraper framework

---
+++ Source Code/Scraper/scraper.py
@@ -0,0 +1,20 @@
+# requires sanction installed
+import urllib2 # for web scraping
+
+### code still in testing ###
+
+# setup
+# for testing, just output to file
+
+# get list of artists
+# global variables
+genius_client_access_token = rnSTyL3ubquxNiWbZLvqe6rf_nHWPsF5W3MZPgTjq4Fzuz7AVk3opkQEK3ALJLZL
+
+## Setup genius
+
+
+
+# for each artist, get link to photo, and list of songs
+
+# for each song, parse lyrics, remove stop words, create word/occurances pairs
+# eventually, pairs should be word/occurances in all songs, w/ list of songs it's in
