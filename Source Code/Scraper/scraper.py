90357994ccb9b28da1fcab0992e563c7712a5544
Alec Schule <aschule@usc.edu>
2017-02-24 12:51:59 -0800

    final scraper version

--- Source Code/Scraper/scraper.py
+++ Source Code/Scraper/scraper.py
@@ -23,12 +23,14 @@ cnx = mysql.connector.connect(**mysql_config) # start connection
 cursor = cnx.cursor()
 
 stops = set(stopwords.words("English")) # english stop words
+stops.update("I")
 
 # Scraping options
 artists_per_page = 1000 # num of artists requested from api per page. Max 1000
 start_page = 1 # artist result page to start scraping on
 end_page = 10 # artist result page to end on
-song_list_delimiter = '$#$' # seperates song names in database
+song_delimiter = '*^*' # seperates song names in database
+frequency_delimiter = '*$*' # seperates song name from frequency
 lyrics_url = "http://localhost:8081/api/find/" # api url for lyrics-api
  
 cursor.execute(get_auto_increment, ("Artist",))
@@ -36,16 +38,18 @@ artist_id = int(cursor.fetchone()[0]) # starting artist id (auto increments)
 
 # base api url
 base_url = "http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=united%20states&limit=" + str(artists_per_page) + "&page=" 
-for page in range(start_page, end_page):
+index_number = artists_per_page*start_page + 1
+for page in range(start_page, end_page+1):
     url = base_url+str(page)+"&api_key=d01318ecdfb319d6bb8ef0ea5895f7a0&format=json"
     response = requests.get(url)
     # Parse response
     artists = response.json()["topartists"]["artist"]
     for artist in artists:
+        index_number += 1
         mbid = artist["mbid"] # id for lookup
         artist_name = artist["name"]
         artist_image = artist["image"][2]["#text"] # image url
-        print(str(artist_id) + " - " + artist_name)
+        print(str(index_number) + " - " + artist_name)
         # get list of songs for artist
         try:
             song_request_url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=" + mbid + "&api_key=d01318ecdfb319d6bb8ef0ea5895f7a0&limit=5&format=json"
@@ -82,27 +86,29 @@ for page in range(start_page, end_page):
                 continue
             lyric_array = re.split("\n| ", lyrics) # split string into tokens by \n and space
             filtered_lyrics = [word.lower() for word in lyric_array if word not in stops] # remove stop words
-            more_filtered_lyrics = [re.sub('["(),\.\?]', '', word) for word in filtered_lyrics]
+            # remove punctuation attatched to words
+            more_filtered_lyrics = [re.sub('["(),:\.\?\!]', '', word) for word in filtered_lyrics]
             lyrics_map = Counter(more_filtered_lyrics) # count word occurances in this song
             for i in lyrics_map: # loop over words to add songs to their tracking
                 current_list = word_song_map.get(i) # current list of songs
                 if word_song_map.get(i) is None:
-                    word_song_map[i] = song_name
+                    word_song_map[i] = song_name + frequency_delimiter + str(lyrics_map[i])
                 else:
-                    word_song_map[i] += song_list_delimiter + song_name
+                    word_song_map[i] += song_delimiter + song_name + frequency_delimiter + str(lyrics_map[i])
             word_count.update(lyrics_map) # update overall count for artist
-        if (word_count.get(' ') is not None):
+        if (word_count.get('') is not None):
             del(word_count['']) # delete empty string which is a result of double spaces in lyrics
-        if (word_song_map.get(' ') is not None):
+        if (word_song_map.get('') is not None):
             del(word_song_map[''])
         try:
             cursor.execute(insert_artist, (artist_name, artist_image)) # add artist to artist table
+            artist_id += 1;
             for i in word_count.keys(): # add all words to word table
-                cursor.execute(insert_song, (i, artist_id, word_count[i], word_song_map[i]))
+                cursor.execute(insert_song, (i, artist_id-1, word_count[i], word_song_map[i]))
         except:
             print("Error adding artist to database. Skipping.")
+            cnx.rollback() # don't commit changes for the failed artist
             continue
         cnx.commit() # commit changes after each artist
-        artist_id += 1;
 cursor.close()
 cnx.close()
