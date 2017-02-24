fcf73e704ad989c37a41c98084301dec4d1b3663
theFOng <alecsanf@usc.edu>
2017-02-23 17:51:36 -0800

    get words, occurences

--- Source Code/backend/WordCloud.php
+++ Source Code/backend/WordCloud.php
@@ -10,13 +10,30 @@ class WordCloud extends DatabaseAccesor
 {
     private $wordCloudData;
 
+    //creates data for word cloud
     public function createWordCloud($artist){
         $id = $this->getArtistID($artist);
-        $findWords = $this->conn->prepare("SELECT TOP 250 * FROM Words WHERE ArtistID = ? 
+        $findWords = $this->conn->prepare("SELECT TOP 250 Word,Occurrences,Songs FROM Words WHERE ArtistID = ? 
                                     ORDER BY Occurences DESC");
         $findWords->bind_param("s", $id);
         $findWords->execute();
-        $this->wordCloudData = $findWords->get_result();
+        $this->wordCloudData = mysqli_fetch_row($findWords->get_result());
     }
+
+    //get words
+    public function getWords(){
+        if (!isset($this->wordCloudData)){
+            return null;
+        }
+        return $this->wordCloudData[0];
+    }
+
+    public function getOccurrences(){
+        if (!isset($this->wordCloudData)){
+            return null;
+        }
+        return $this->wordCloudData[1];
+    }
+
 
 }
