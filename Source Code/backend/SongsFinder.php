59ce67b34d076a33976989d6d5ba0f38ff1d258e
Andrew Zolintakis <peterzolintakis@Peters-MacBook-Pro-3.local>
2017-02-26 15:15:03 -0800

    SongsFinder working

--- Source Code/backend/SongsFinder.php
+++ Source Code/backend/SongsFinder.php
@@ -1,30 +1,34 @@
 <?php
 
+include "DatabaseAccesor.php";
+
 // Class will get songs from database that are by a certain artist and contains a certain word
 class SongsFinder extends DatabaseAccesor
 {
 
 
     // Gets the songs using an artistID and a word from the DB
-    public function getSongs($partialName, $artistName){
+    public function getSongs($word, $artistName){
         $artistID = $this->getArtistID($artistName);
         if($artistID == null){
             return null;
         }
         $songsStatement = $this->conn->prepare("SELECT Songs FROM Word WHERE Word = ? AND ArtistID = ?");
-        $songsStatement->bind_param("si", $partialName, $artistName);
+        $songsStatement->bind_param("si", $word, $artistID);
         $songsStatement->execute();
         $result = $songsStatement->get_result();
         if ($result->num_rows > 0) {
             // output data of each row
             if ($row = $result->fetch_assoc()) {
                 $songsFrequencyString = $row["Songs"];
-                $songsFrequencyArray = explode(",", $songsFrequencyString);
+                $songsFrequencyArray = explode("*^*", $songsFrequencyString);
                 $songsFrequencyAssociativeArray = array();
-                for ($i = 0; $i <= count($songsFrequencyArray) / 2; $i += 2) {
-                    $songsFrequencyAssociativeArray[$songsFrequencyArray[$i]] = $songsFrequencyArray[$i + 1];
+                for ($i = 0; $i < count($songsFrequencyArray); $i ++) {
+                    $singleSong = explode("*$*", $songsFrequencyArray[$i]);
+                    $songsFrequencyAssociativeArray[$singleSong[0]] = $singleSong[1];
                 }
                 arsort($songsFrequencyAssociativeArray);
+                print_r($songsFrequencyAssociativeArray);
                 $songsStatement->close();
                 return $songsFrequencyAssociativeArray;
             }
@@ -33,7 +37,7 @@ class SongsFinder extends DatabaseAccesor
         return null;
 
     }
-
-
 }
+
+
 
