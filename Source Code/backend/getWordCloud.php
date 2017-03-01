f916066679fc541d010a9b657f94379e6d3ed31b
theFOng <alecsanf@usc.edu>
2017-03-01 09:52:52 -0800

    optimized readability and run time for getwordcloud

--- Source Code/backend/getWordCloud.php
+++ Source Code/backend/getWordCloud.php
@@ -6,72 +6,43 @@
  * Time: 6:17 PM
  */
 include "WordCloud.php";
-//$artistsCSV =  "Drake,Coldplay"; *EXAMPLE*
-$artistsCSV = $_GET["artists"];
+//$artistsCSV =  "Drake,Coldplay,Rihanna,Kendrick Lamar,Radiohead"; *EXAMPLE*
+//$artistsCSV = $_GET["artists"];
+$artistsCSV =  "Drake,Coldplay,Rihanna,Kendrick Lamar,Radiohead";
 $artists = str_getcsv($artistsCSV);
 
 if ($artists == null){
     echo null;
     exit(1);
 }
-$sendObj = array();
 
-//put first artist in sendObj
-$wc = new WordCloud();
-$wc->createWordCloud($artists[0]);
-$words = $wc->getWords();
-$sendObj = array();
-while($row = $words->fetch_assoc()){
-    array_push($sendObj,array("text"=>$row["Word"],"size"=> $row["Occurences"]));
-}
-
-//for all artist
-for ($i = 1; $i < count($artists); $i++){
-    $wc2 = new WordCloud();
-    $wc2->createWordCloud($artists[$i]);
-    $words2 = $wc2->getWords();
+$wordToCountHM = array();
+//for all the artists
+for ($i = 0; $i < count($artists); $i++){
+    $wc = new WordCloud();
+    $wc->createWordCloud($artists[$i]);
+    $words = $wc->getWords();
     //for all words
-    while($row = $words2->fetch_assoc()){
-        //check and add against current list
-        $found = false;
-        $insertPos = 0;
-        for($c = 0; $c < count($sendObj); $c++){
-            // if word
-            if ($sendObj[$c]["text"] == $row["Word"]){
-                (int)$sendObj[$c]["size"] = (int)$sendObj[$c]["size"]+(int)$row["Occurences"];
-                $found = true;
-            }
-            // if current occurrence is less than one in list save pos
-            if ((int)$row["Occurences"] < (int)$sendObj[$c]["size"]){
-                $insertPos = $c;
-            }
-        }
-        // if not found, insert in order of occurrence
-        if (!$found){
-            array_splice($sendObj, $insertPos, 0,array(array("text"=>$row["Word"],"size"=> $row["Occurences"])));
+    while($row = $words->fetch_assoc()){
+        //put in hash or add
+        if ($count = $wordToCountHM[$row["Word"]]){
+            $wordToCountHM[$row["Word"]] = $count + $row["Occurences"];
+        } else {
+            $wordToCountHM[$row["Word"]] = $row["Occurences"];
         }
     }
 }
+//sort in descending order
+arsort($wordToCountHM);
 
-//using bubble sort for optimal sorting of almost sorted lists
-$size = count($sendObj);
-for ($i=0; $i<$size; $i++) {
-    for ($j=0; $j<$size-1-$i; $j++) {
-        if ((int)$sendObj[$j+1]["size"] > (int)$sendObj[$j]["size"]) {
-            swap($sendObj, $j, $j+1);
-        }
-    }
+$sendObj = array();
+//put in format
+foreach ($wordToCountHM as $key => $value){
+    array_push($sendObj,array("text"=>$key,"size"=> $value));
 }
 
 //return 250
 $sendObj =  array_chunk($sendObj,250)[0];
 
 echo json_encode($sendObj);
-
-// used for swapping in bubble sort
-function swap(&$arr, $a, $b) {
-    $tmp = $arr[$a];
-    $arr[$a] = $arr[$b];
-    $arr[$b] = $tmp;
-}
 ?>
