9948ae45aff8e254cd64e5b13f67ca733faaec0c
Noah Bergman <nabergma@usc.edu>
2017-03-02 12:32:59 -0800

    hey

--- Source Code/frontEnd/javascript.js
+++ Source Code/frontEnd/javascript.js
@@ -230,7 +230,7 @@ function populateLyrics(lyrics, artist, word){ //void
 	
 	var lyric = String(lyrics);
 	var inner = lyrics.replace(new RegExp(" " +word + " ", "g"), '<span style="color:yellow"> ' + word + '</span>');
-	inner = lyrics.replace(new RegExp(" " +word + ",", "g"), '<span style="color:yellow"> ' + word + ',</span>');
+	inner = inner.replace(new RegExp(" " +word + ",", "g"), '<span style="color:yellow"> ' + word + ',</span>');
 	inner = currentSong + "<br> <br>" + inner;
      var theDiv = document.getElementById("Lyrics");
 	theDiv.innerHTML = inner; 
