0c7e189eb1d701d588199ec7b61cb31a261cc35a
theFOng <alecsanf@usc.edu>
2017-03-08 16:12:47 -0800

    adding tests

---
+++ Source Code/backend/tests/getWordCloudTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers WordCloud
+ */
+final class GetWordCloudTest extends TestCase
+{
+    public function testNullArray(){
+        $wc->
+    }
+
+}
